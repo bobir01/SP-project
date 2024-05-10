129f269c68a73a68111790b12b1e863ca414e09f
Bobirjon <mardonovbobir9@gmail.com>
2024-05-10 10:16:25 +0500

    ps objects updated to return dict

--- ps_utils.py
+++ ps_utils.py
@@ -1,45 +1,40 @@
 import psutil
-import os
-import sys
-import time
-from pathlib import Path
-from typing import List, Dict, Any, Union
-from decimal import Decimal
-import  pprint
+from typing import List, Dict, Any
+
+import pprint
 import re
 
 
-# class that will be generating the process list and managing the process #fixme add the process manager funct
 class ProcessManager:
     def __init__(self):
-        self.processes: List[psutil.Process] = []
+        self.processes: List[Dict] = []
         self.process_attributes = ['pid', 'name', 'status', 'cpu_percent', 'memory_percent', 'create_time',
-                                   'exe', 'cmdline', 'nice', 'username', 'num_threads']
+                                   'exe', 'nice', 'username', 'num_threads', 'cmdline', ]
 
-    def get_processes(self) -> List[psutil.Process]:
+    def get_processes(self) -> List[Dict[str, Any]]:
         """Get list of all processes running on the system."""
         self.processes = []
         for proc in psutil.process_iter(attrs=self.process_attributes):
             try:
-                self.processes.append(proc)
+                self.processes.append(proc.as_dict(attrs=self.process_attributes))
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                pass
-        return self.processes
+                continue
+        return sorted(self.processes, key=lambda x: x['pid'], reverse=True)
 
-    def get_process_by_pid(self, pid: int) -> psutil.Process:
+    def get_process_by_pid(self, pid: int) -> List[Dict[str, Any]]:
         """Get process by PID.
         :param pid: Process ID.
         :return: Process object.
+        """
+        patrn = re.compile(f'{pid}', flags=re.IGNORECASE)
+        return [proc for proc in self.processes if patrn.match(str(proc.get('pid')))]
 
-        :raises: NoSuchProcess"""
-        return psutil.Process(pid)
-
-    def get_process_by_name(self, name: str) -> List[psutil.Process]:
+    def get_process_by_name(self, name: str) -> List[Dict[str, Any]]:
         """Get list of processes by name.
         :param name: Process name.
         :return: List of process objects."""
-        re_pattern = re.compile(name)
-        return [proc for proc in self.processes if re_pattern.match(proc.name())]
+        re_pattern = re.compile(name, flags=re.IGNORECASE)
+        return [proc for proc in self.processes if re_pattern.match(proc['name'])]
 
     def renice_process(self, pid: int, nice: int) -> None:
         """Change the priority of a process.
@@ -53,9 +48,6 @@ if __name__ == '__main__':
     pm = ProcessManager()
     pcs = pm.get_processes()
     # pcs[-1].kill()
-    pprint.pprint(pcs[-1].as_dict(attrs=pm.process_attributes))
+    pprint.pprint(pcs[0])
     # kill the last process-2
-    print(pcs[-2].pid)
-    pcs[-2].kill()
-    pm.get_process_by_pid(12194).kill()
     print("done")
