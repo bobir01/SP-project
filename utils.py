2db2614e2569ef2a33cd2ef2eb2ed64bf4d4bf02
Bobirjon <mardonovbobir9@gmail.com>
2024-05-13 08:19:33 +0500

    utils timezone added

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from datetime import timezone, timedelta
 
-
-def uni2dt(uni: float) -> datetime:
-    """Convert a Unix timestamp to a datetime object.
-    :param uni: Unix timestamp.
+def uni2dt(unix: float) -> datetime:
+    """Convert a Unix timestamp to a datetime object with UTC+5
+    :param unix: Unix timestamp.
     :return: Datetime object."""
-    return datetime.utcfromtimestamp(uni)
+
+    return datetime.utcfromtimestamp(unix).replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=5),
+                                                                                             name='UTC+5'))
 
 def format_float(f: float = None) -> str:
     """Format a float to a string with a given precision.
